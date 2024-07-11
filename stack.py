d8f93ff18a1b226d5a4aa5c0266c0a9bfea43aaf
dananasr2004 <dananasr871@gmail.com>
2024-07-11 13:55:56 +0300

    push to stack

--- stack.py
+++ stack.py
@@ -2,3 +2,10 @@ class Stack:
   def __init__(self):
     self.head = None
     self.size = 0
+  def push(self, node):
+    
+    print("We are pushing:", node.user, "\n")
+
+    node.next = self.head
+    self.head = node
+    self.size += 1
