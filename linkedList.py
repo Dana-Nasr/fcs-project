9f6719066776ec39e5089487ea4ab8e2976493f0
dananasr2004 <dananasr871@gmail.com>
2024-07-11 13:29:40 +0300

    add node to list

--- linkedList.py
+++ linkedList.py
@@ -1,5 +1,11 @@
 class LinkedList:
 
-  def __init__(self):
+   def __init__(self):
     self.head = None
-    self.tail = None
+    self.size = 0
+  
+   def addNode(self, data):
+    new_node = Node(data)
+    new_node.next = self.head
+    self.head = new_node
+    self.size += 1
