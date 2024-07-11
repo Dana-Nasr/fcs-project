72d1938e39116d13f2dac3fb31fb0bafee539ce3
dananasr2004 <dananasr871@gmail.com>
2024-07-11 13:01:43 +0300

    list constructor

--- node.py
+++ node.py
@@ -1,5 +1,5 @@
 class Node:
     def __init__(self, user, weight=1, next=None):
-        self.user = user  # User object
-        self.weight = weight  # Weight associated with the node
-        self.next = next  # Reference to the next node in the linked list
+        self.user = user                # User object
+        self.weight = weight            # Weight associated with the node
+        self.next = next                # Reference to the next node in the linked list
