8de77b03c6b7f1a7e992527a12c82a7a97480528
dananasr2004 <dananasr871@gmail.com>
2024-07-11 14:02:20 +0300

    get queue size

--- queu.py
+++ queu.py
@@ -2,3 +2,10 @@ class queue:
  def __init__(self):
     self.head = None
     self.tail = None
+ def getSize(self):
+    current = self.head
+    size = 0
+    while current != None:
+      size += 1
+      current = current.next
+    return size
