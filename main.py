1142033bb45a819c057130bbfd1d1f0d2c761a6f
dananasr2004 <dananasr871@gmail.com>
2024-07-11 16:27:30 +0300

    create users

---
+++ main.py
@@ -0,0 +1,6 @@
+from socialPlatform import Graph
+from user import User
+
+user1 = User(1, "Hani", ["Math", "Science"], {"interested_in_topics": ["Physics"], "already_known_topics": ["Algebra"]})
+user2 = User(2, "Salam", ["History"], {"interested_in_topics": ["Math"], "already_known_topics": ["Ancient History"]})
+user3 = User(3, "Fadi", ["Computer Science", "Physics"], {"interested_in_topics": ["Algebra"], "already_known_topics": ["Gravity"]})
