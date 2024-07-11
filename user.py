ac6250fcd3b444e9b36031367d2e54aeb0509840
dananasr2004 <dananasr871@gmail.com>
2024-07-11 10:25:51 +0300

    user constructor

---
+++ user.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self, id, name, subjects, topics):  ##list subject dict topics
+        self.id = id
+        self.name = name
+        self.subjects = subjects
+        self.topics = topics
+
+   
+    
