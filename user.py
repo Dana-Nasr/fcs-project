c2faa15b8dbc389c414cc8e0fff0b444c5269553
dananasr2004 <dananasr871@gmail.com>
2024-07-11 11:14:32 +0300

    addInterestedTopic

--- user.py
+++ user.py
@@ -1,5 +1,5 @@
 class User:
-    def __init__(self, id, name, subjects, topics):  ##list subject dict topics
+    def __init__(self, id, name, subjects, topics):
         self.id = id
         self.name = name
         self.subjects = subjects
@@ -9,20 +9,19 @@ class User:
         if subject not in self.subjects:
             self.subjects.append(subject)
 
-    def addInterestedTopic(self, topic):                ##key 1 in dict
+    def addInterestedTopic(self, topic):                 ##key 1 in dict
         if 'interested_in_topics' not in self.topics:
             self.topics['interested_in_topics'] = []
         if topic not in self.topics['interested_in_topics']:
             self.topics['interested_in_topics'].append(topic)
 
-
-    def addKnowntTopic(self, topic):                    ##key 2 in dict
+    def addKnownTopic(self, topic):                      ##key 2 in dict
         if 'already_known_topics' not in self.topics:
             self.topics['already_known_topics'] = []
         if topic not in self.topics['already_known_topics']:
             self.topics['already_known_topics'].append(topic)   
-    def printUser():
-        print("id={self.id}, name={self.name}, subjects={self.subjects}, topics={self.topics}")
+
+    def printUser(self):
+        print(f"id={self.id}, name={self.name}, subjects={self.subjects}, topics={self.topics}")  #f to evaluate inside the {}
 
 
-        
