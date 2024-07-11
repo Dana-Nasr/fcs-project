b15b595375b9d1b2c88caafbae4184eb7a385d28
dananasr2004 <dananasr871@gmail.com>
2024-07-11 12:53:53 +0300

    class node and its constructor

---
+++ node.py
@@ -0,0 +1,5 @@
+class Node:
+    def __init__(self, user, weight=1, next=None):
+        self.user = user  # User object
+        self.weight = weight  # Weight associated with the node
+        self.next = next  # Reference to the next node in the linked list
