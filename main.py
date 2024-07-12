61584df0fc9573d4324e56b925b3297b206449d9
dananasr2004 <dananasr871@gmail.com>
2024-07-12 09:56:35 +0300

    main function

--- main.py
+++ main.py
@@ -1,23 +1,60 @@
 from socialPlatform import Graph
 from user import User
 from node import Node
-
+from stack import Stack
+from queu import Queue
   
 def main():
-    user1 = User(1, "Hani", ["Math", "Science"], {"interested_in_topics": ["Physics"], "already_known_topics": ["Algebra"]})
-    user2 = User(2, "Salam", ["History"], {"interested_in_topics": ["Math"], "already_known_topics": ["Ancient History"]})
-    user3 = User(3, "Fadi", ["Computer Science", "Physics"], {"interested_in_topics": ["Algebra"], "already_known_topics": ["Gravity"]})
+    # Create Users
+    user1 = User(1, "sara", ["Math", "Science"], {})
+    user2 = User(2, "heba", ["History", "Art"], {})
+    user3 = User(3, "fadi", ["Math", "Art"], {})
+    user4 = User(4, "dalal", ["Science", "History"], {})
+
+    # Print Users
+    user1.printUser()
+    user2.printUser()
+    user3.printUser()
+    user4.printUser()
+
+    # Modify Users
+    user1.addSubject("Physics")
+    user2.addInterestedTopic(" Mechanics")
+    user3.addKnownTopic(" Art")
+    user4.changeName("david nasr")
+
+    # Print Modified Users
+    user1.printUser()
+    user2.printUser()
+    user3.printUser()
+    user4.printUser()
+
+    # Create Graph
+    graph = Graph()
+
+    # Add vertices
+    graph.addVertex("sara")
+    graph.addVertex("heba")
+    graph.addVertex("fadi")
+    graph.addVertex("david")
+
+    # Add edges
+    graph.addFriendEdge("sara", "heba", 5)
+    graph.addFriendEdge("fadi", "sara", 3)
+    graph.addFollowEdge("david", "sara")
+
+    # Display Graph
+    graph.displayGraph()
+
+    # DFS Traversal
+    print("DFS Traversal from sara:")
+    graph.dfs("sara")
+
+    # BFS Traversal
+    print("BFS Traversal from sara:")
+    graph.bfs("sara")
+
     
 
-    #  node1=Node(user1,0)
-    #  node2=Node(user2,0)
-    #  node3=Node(user3,0)
 
-
-    graph = Graph()
-    graph.addVertex(user1)
-    graph.addVertex(user2)
-    graph.addVertex(user3)
-
-    graph.displayGraph()
 main()
