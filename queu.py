9c1f1b3b6dfd78c30bf1fe07121894c6f3006c29
dananasr2004 <dananasr871@gmail.com>
2024-07-12 16:26:06 +0300

    added else to inqueu in queue

--- queu.py
+++ queu.py
@@ -1,34 +1,35 @@
 class Queue:
- def __init__(self):
-    self.head = None
-    self.tail = None
- def getSize(self):
-    current = self.head
-    size = 0
-    while current != None:
-      size += 1
-      current = current.next
-    return size
- def inqueue(self, node):
-    if self.head == None:
-      self.head = node
-      self.tail = node
-    else:
-      self.tail.next = node
-      self.tail = node          
-
- def dequeue(self):
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def getSize(self):
+        current = self.head
+        size = 0
+        while current is not None:
+            size += 1
+            current = current.next
+        return size
+    
+    def inqueue(self, node):
+        if self.head is None:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+    
+    def dequeue(self):
         if self.head is None:
             print("Queue is empty")
             return None
         removed_node = self.head
         self.head = self.head.next
-        if self.head is None:       
-            self.tail = None
-        return removed_node.user
- 
- def displayNodes(self):
-    current = self.head
-    while current != None:
-      print(current.user)
-      current = current.next
+        removed_node.next = None  
+        return removed_node
+    
+    def displayNodes(self):
+        current = self.head
+        while current is not None:
+            print(current.user.id)
+            current = current.next
