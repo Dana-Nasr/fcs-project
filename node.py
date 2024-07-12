572b1531acb30022319fd0b2db752e5a507834d7
dananasr2004 <dananasr871@gmail.com>
2024-07-12 09:35:25 +0300

    assigning next to none

--- node.py
+++ node.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, user, weight, next):
+    def __init__(self, user, weight, next=None):
         self.user = user                # User object
         self.weight = weight            # Weight associated with the node
-        self.next ==None                # Reference to the next node in the linked list
+        self.next = next                # Reference to the next node in the linked list
