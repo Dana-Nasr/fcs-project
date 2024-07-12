a6cde2ce8c14d62da73c15534e2dfbdb4bc73041
dananasr2004 <dananasr871@gmail.com>
2024-07-12 16:40:11 +0300

    main

--- main.py
+++ main.py
@@ -8,28 +8,24 @@ def main():
     
     graph = Graph()
     
-    graph.addUser(1, "sara", subjects=['math', 'science'], topics={})
-    graph.addUser(2, "leen", subjects=['history', 'math'], topics={})
-    graph.addUser(3, "samer", subjects=['math', 'english'], topics={})
-    graph.addUser(4, "hadi", subjects=['science', 'history'], topics={})
+    user1 = User(1, "sara", ['math', 'Science'], {'interested_in_topics': ['geometry']})
+    user2 = User(2, "lana", ['history', 'math'], {'interested_in_topics': ['acient eygpt']})
+    user3 = User(3, "heba", ['math', 'art'], {'interested_in_topics': ['geometry']})
+    user4 = User(4, "dani", ['science', 'english'], {'interested_in_topics': []})
     
-    print("Initial Graph:")
-    graph.displayGraph()
-  
-  
+    graph.addUser(user1)
+    graph.addUser(user2)
+    graph.addUser(user3)
+    graph.addUser(user4)
+
     graph.addFriendEdge(1, 2, 4)
     graph.addFriendEdge(1, 3, 2)
     graph.addFriendEdge(2, 4, 1)
 
-  
     graph.displayGraph()
-
 
     graph.recommendFriends(1)
 
-   
-    print("DFS Traversal from sara (vertex 1):")
+    print("DFS Traversal from sara:")
     graph.dfs(1)
-    
-    
 main()
